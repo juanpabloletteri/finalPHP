099de180650e3bd9b119c23b48d391955a45cb95
juanpabloletteri <pabloletteri@gmail.com>
2017-06-29 20:46:16 -0300

    navbar

---
+++ funciones.js
@@ -0,0 +1,7 @@
+function botones(){
+    alert("botones")
+}
+
+function tabla(){
+    alert("tabla")
+}
--- index.php
+++ index.php
@@ -27,9 +27,10 @@
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
+      <li><a onclick="botones()" href="#">BOTONES</a></li>
+      <li><a onclick="tabla()" href="#">TABLA</a></li>
     </ul>
+      
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
       <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
